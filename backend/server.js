52c56028e48031be98d946d8d088dee45619b193
X <maxmensmac@maxmens-MBP.home>
2025-06-25 10:13:00 +0200

    Add back express.json and express.urlencoded middleware for isolation step

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.get('/', (req, res) => {
   res.send('Hello World from main app');
