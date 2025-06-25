e52734128d1107558b3754f85bff9cbea71e3c3b
X <maxmensmac@maxmens-MBP.home>
2025-06-25 10:04:02 +0200

    Add back helmet middleware for isolation step

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const websiteRoutes = require('./routes/websites');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
+
 app.get('/', (req, res) => {
   res.send('Hello World from main app');
 });
