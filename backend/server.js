d04ac6aef8dbd9f0d6024b40ead4919f229a649a
X <maxmensmac@maxmens-MBP.home>
2025-06-25 10:18:07 +0200

    Add back morgan logging middleware for isolation step

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello World from main app');
