110f7d4c6fd20532eccd5823fed407615f44e0ae
X <maxmensmac@maxmens-MBP.home>
2025-06-25 10:22:39 +0200

    Add back static file serving middleware for isolation step

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World from main app');
